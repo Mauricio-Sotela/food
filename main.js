038cd8c0dc1f7146ad90719eb7d9c4eafe72e820
sotela88 <j-ms-27@hotmail.com>
2020-03-16 22:13:10 +0100

    food

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 function getcube() {
   return (name = document.getElementById("artis").value);
 }
-var contenido = document.querySelector("#contenido");
+let contenido = document.getElementById("contenido");
 b=function traer() {
   fetch("https://api.edamam.com/search?q="+getcube()+"&app_id=ce448bd7&app_key=02a720f7a7b52cd92713f617784d5bae&from=0&to=3&calories=591-722&health=alcohol-free")
     .then(res => res.json())
     .then(data => {
       console.log(data);
-
+     contenido.innerHTML="";
       let h = document.createElement("H2");
       let t = document.createTextNode(data["hits"][0]["recipe"]["label"]);
       h.appendChild(t);
-      document.body.appendChild(h);
+      contenido.appendChild(h);
 
       let img=data["hits"][0]["recipe"]["image"];
       let myImage = new Image;
       myImage.src = img;
-      document.body.appendChild(myImage);
+      contenido.appendChild(myImage);
 
 
       let list=data["hits"][0]["recipe"]["ingredientLines"];
@@ -28,7 +28,7 @@ b=function traer() {
         let o = document.createElement("P");
         let p = document.createTextNode(list[i]);
         o.appendChild(p);
-        document.body.appendChild(o); 
+        contenido.appendChild(o); 
       }
      
      
