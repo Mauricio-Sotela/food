91e457b7241ae4de7171baa9b79d7515ca2d9d37
sotela88 <j-ms-27@hotmail.com>
2020-03-16 21:34:29 +0100

    food

---
+++ main.js
@@ -0,0 +1,42 @@
+function getcube() {
+  return (name = document.getElementById("artis").value);
+}
+var contenido = document.querySelector("#contenido");
+b=function traer() {
+  fetch("https://api.edamam.com/search?q="+getcube()+"&app_id=ce448bd7&app_key=02a720f7a7b52cd92713f617784d5bae&from=0&to=3&calories=591-722&health=alcohol-free")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+
+      let h = document.createElement("H2");
+      let t = document.createTextNode(data["hits"][0]["recipe"]["label"]);
+      h.appendChild(t);
+      document.body.appendChild(h);
+
+      let img=data["hits"][0]["recipe"]["image"];
+      let myImage = new Image;
+      myImage.src = img;
+      document.body.appendChild(myImage);
+
+
+      let list=data["hits"][0]["recipe"]["ingredientLines"];
+
+      
+
+      let ingredients = document.createElement("H2");
+      for (let i = 0; i < list.length; i++) {
+        let tx = document.createTextNode(list[i]);
+         ingredients.appendChild(tx);
+      }
+     
+      document.body.appendChild(ingredients);
+    
+      
+
+ 
+    });
+
+    
+}
+
+
